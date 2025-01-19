9e820be63498fcd3c44b92a2d3df668a8371fbb0
Kamrul Hasan Tusher <kmtusher97@gmail.com>
2025-01-19 22:58:35 +0600

    Revert "[Issue 04] Add Configuration for Tailwind css and add profile cards" (#12)
    * Revert "Rename App Title"
    
    This reverts commit dfc74bda319020b96f6e121094afd04a650ded95.
    
    * Revert "Add propTypes Verification"
    
    This reverts commit 80fd9d32f1c4ccf1539e8da374b769e94ebd2230.
    
    * Revert "add profile cards"
    
    This reverts commit fdd3a0988e98499ddf46900181bd4fafd739663e.
    
    * Revert "Configure Tailwind css"
    
    This reverts commit ce2d294c633a8299680042aac236b29fd4d6d3f8.

--- src/Components/Images.jsx
+++
@@ -1,7 +0,0 @@
-export function Images({source, alterText}) {
-  return <img className="w-24 h-24 rounded-xl" src={source} alt={alterText} />;
-}
-Images.propTypes = {
-  source: PropTypes.string.isRequired,
-  alterText: PropTypes.string.isRequired,
-};
